525105cda81861ad338082831b5862fe3151198f
Martin Lidgren <marlidg@student.chalmers.se>
2025-10-06 19:00:51 +0200

    Update run.py and init to connect to database

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
+    # Database configuration
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    # Print for debugging (remove later)
+    print("Using database URL:", app.config["SQLALCHEMY_DATABASE_URI"])
+
+    db.init_app(app)
+
     from .routes import main
     app.register_blueprint(main)
+
+    # Import models
+    from .models import movies
 
     return app
